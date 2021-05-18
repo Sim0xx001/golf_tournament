9327e5345af70a13146a0962b847b568f0650384
Simone Mammoro <simoxodi@gmail.com>
2021-05-18 20:11:43 +0200

    General commit

--- main.cpp
+++ main.cpp
@@ -70,6 +70,7 @@ void readFile(string file, vector<player>* ptr) {
 	}
 }
 
+//sas
 // Print players according to sorted points
 void print_sort(vector<player>* ptr, bool sort) {
 	bool sorted = false;
